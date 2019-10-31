2a4aa944898e49612b0376aab70fa7b3976db55c
tspola <tjspolar@gmail.com>
2019-10-30 20:25:32 -0700

    age was not defined

--- lesson8.js
+++ lesson8.js
@@ -9,7 +9,8 @@ var people = [
 var nameFilter = function() {
     for (var i = 0; i < people.length; i++) {
         let name = people[i].name
-        if (name > 50) {
+        let age = people[i].age
+        if (age < 50) {
             console.log("My Name is " + name)
         }
     }
