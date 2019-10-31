87817c8296f0fb68eedda27b4fbd74e2d5cb096a
tspola <tjspolar@gmail.com>
2019-10-30 20:01:25 -0700

    added names

--- lesson1.js
+++ lesson1.js
@@ -1,6 +1,6 @@
 //Should console.log all names in the name array
-
+var names = ["pedro", "john"];
 for (var i = 0; i < names.length; i++) {
     console.log("My Name is " + names[i])
 }
-let names = ["pedro", "john"]
+// let names = ["pedro", "john"]
